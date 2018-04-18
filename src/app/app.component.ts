b9787ecff41122c1557c88dd303cb9f12504aa4e
JustinRoyTamaela <jrtamaela@gmail.com>
2018-04-18 11:09:06 +0200

    merge conflicts opgelost

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-<<<<<<< HEAD
 
 export class AppComponent {
   title = 'app';
-
-=======
-export class AppComponent {
-  title = 'ReminderView';
->>>>>>> 5187ad02d36c302f2d1b5a00974ee63b7c22e7a4
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-
-
-import { AppComponent } from './app.component';
 import { ClientenBestandComponent } from './clienten-bestand/clienten-bestand.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { VasteDagtakenComponent } from './vaste-dagtaken/vaste-dagtaken.component';
-=======
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
@@ -17,32 +11,20 @@ import { AppComponent } from './app.component';
 import { RemindersComponent } from './reminders/reminders.component';
 import { ReminderService } from './reminder.service';
 
->>>>>>> 5187ad02d36c302f2d1b5a00974ee63b7c22e7a4
-
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     ClientenBestandComponent,
     DashboardComponent,
     VasteDagtakenComponent,
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  providers: [
-  ],
-=======
     RemindersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
   ],
   providers: [ReminderService],
->>>>>>> 5187ad02d36c302f2d1b5a00974ee63b7c22e7a4
   bootstrap: [AppComponent]
 })
 export class AppModule { }
